64c347b4e4efb8bafd4704b4416e036581db7396
fidelscodes <fidelscodes@gmail.com>
2016-09-28 19:04:43 -0400

    Done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_or_create_by_omniauth(auth_hash)
+    session[:user_id] = user.id
+    
+    redirect_to root_path
+  end
+
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    # finding by uid instead of email since the tests check for uid first
+    where(uid: auth_hash[:uid]).first_or_create do |user|
+      user.name = auth_hash[:info][:name]
+      user.email = auth_hash[:info][:email]
+    end
+  end
+
 end
