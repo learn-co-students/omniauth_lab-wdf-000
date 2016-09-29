1e2a5fdd410ec421f28c252195c536a13bc36e94
ozPop <ozziepopovas@gmail.com>
2016-09-29 10:25:19 -0400

    add migration, update sessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160929141945) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "uid"
+    t.text     "info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "email"
+  end
 
 end
