ca018d4fb87031d626b663558c9caa5837d2dd89
ozPop <ozziepopovas@gmail.com>
2016-09-29 10:07:53 -0400

    add sessions controller, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   root 'welcome#home'
 
   get '/:user/index', to: 'users#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
+  get '/auth/facebook/callback' => 'sessions#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
